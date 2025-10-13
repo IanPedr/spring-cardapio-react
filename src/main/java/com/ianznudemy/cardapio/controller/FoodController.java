6312bbeb07c11e1b9c1f74e4a965d8189e859b17
IanPedr <iamnascimento2@gmail.com>
2025-10-13 10:42:57 -0300

    backend created

--- src/main/java/com/ianznudemy/cardapio/controller/FoodController.java
+++ src/main/java/com/ianznudemy/cardapio/controller/FoodController.java
@@ -1,11 +1,36 @@
 package com.ianznudemy.cardapio.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ianznudemy.cardapio.food.Food;
+import com.ianznudemy.cardapio.food.FoodRepository;
+import com.ianznudemy.cardapio.food.FoodRequestDTO;
+import com.ianznudemy.cardapio.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("food")
 public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data) {
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+    }
 
 }
---
+++ src/main/java/com/ianznudemy/cardapio/food/FoodRequestDTO.java
@@ -0,0 +1,4 @@
+package com.ianznudemy.cardapio.food;
+
+public record FoodRequestDTO(String title,String image,Integer price) {
+}
